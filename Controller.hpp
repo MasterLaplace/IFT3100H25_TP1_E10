304e7feb6be93504e5c1b5d8595e0795b1aeafb0
Renaud <renaudtremblay@outlook.fr>
2025-02-07 16:30:22 -0500

    J'ajoute une classe Controller.cpp vide que je vais remplir prochainement. JE suis juste content d'avoir réglé mon problème technique.

---
+++ Controller.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+// J'ai rien fait encore avec cette classe la mais je suis juste content que ofxImGui fonctionne.
+
+#include "ofMain.h"
+#include "ofxImGui.h"
+
+class Controller {};
