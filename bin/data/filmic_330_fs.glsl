dc0c68b9a7e9b299745e52c4df8150920f6c0468
Craignosite <155870074+Craignosite@users.noreply.github.com>
2025-03-26 14:29:10 -0400

    Shaders et Arial
    Je les ajoute à la main parce que j'étais incapable de la faire autrement.

---
+++ bin/data/filmic_330_fs.glsl
@@ -0,0 +1,51 @@
+// IFT3100H24 ~ tone_mapping_330_fs.glsl
+
+#version 330
+
+// attribut en entrée
+in vec2 surface_texcoord;
+
+// attribut en sortie
+out vec4 fragment_color;
+
+// attributs uniformes
+uniform sampler2DRect image;
+uniform float exposure;
+uniform float gamma;
+
+// mappage tonal de la couleur hdr vers ldr (approximation de la courbe du ACES filmic tone mapping)
+vec3 tone_mapping_aces_filmic(vec3 x)
+{
+  float a = 2.51f;
+  float b = 0.03f;
+  float c = 2.43f;
+  float d = 0.59f;
+  float e = 0.14f;
+  return clamp((x * (a * x + b)) / (x * (c * x + d) + e), 0.0, 1.0);
+}
+
+void main()
+{
+  // variable qui va contenir la couleur à corriger
+  vec3 color;
+
+  // échantillonner la texture
+  vec3 texture_sample = texture(image, surface_texcoord).rgb;
+
+  // valeur initiale de la couleur
+  color = texture_sample;
+
+  // conversion de la couleur de l'espace gamma vers l'espace linéaire
+  color = pow(color, vec3(gamma));
+
+  // ajustement de l'exposition
+  color = vec3(1.0) - exp(-color * exposure);
+
+  color = tone_mapping_aces_filmic(color);
+
+  // conversion de couleur de l'espace linéaire vers l'espace gamma
+  color = pow(color, vec3(1.0 / gamma));
+
+  // couleur du fragment
+  fragment_color = vec4(color, 1.0);
+}
