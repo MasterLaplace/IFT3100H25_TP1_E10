bf3fd1a538b7717ccce7f2f65bd59c31bb88d0df
Thomas Ouellet <thomasouellet0@gmail.com>
2025-04-29 01:47:56 -0400

    Displacement done and normal mapping is WIP

--- src/MappingScene.cpp
+++ src/MappingScene.cpp
@@ -13,41 +13,59 @@ void MappingScene::setup()
     camera.setDistance(600);
     camera.setNearClip(10);
     camera.setFarClip(2000);
-
     rotation = ofQuaternion(0, 0, 0, 1);
     currentRotation = ofQuaternion(0, 0, 0, 1);
     dragging = false;
 
+    // Charger les textures
     ofLoadImage(diffuseTexture, "mapping/diffuse.jpg");
     ofLoadImage(normalTexture, "mapping/normal.jpg");
     ofLoadImage(heightMap, "mapping/height.jpg");
+
+    // S'assurer que heightMap est en niveaux de gris
+    // Cela pourrait aider avec le problème de pics
+    ofPixels pixels;
+    heightMap.readToPixels(pixels);
+    if (pixels.getNumChannels() > 1)
+    {
+        ofPixels grayPixels;
+        grayPixels.allocate(pixels.getWidth(), pixels.getHeight(), OF_PIXELS_GRAY);
+        for (int y = 0; y < pixels.getHeight(); y++)
+        {
+            for (int x = 0; x < pixels.getWidth(); x++)
+            {
+                ofColor color = pixels.getColor(x, y);
+                float gray = color.getBrightness();
+                grayPixels.setColor(x, y, ofColor(gray));
+            }
+        }
+        heightMap.allocate(grayPixels);
+    }
+
+    // Configurer les paramètres de texture pour la heightMap
+    heightMap.setTextureMinMagFilter(GL_LINEAR, GL_LINEAR);
+    heightMap.setTextureWrap(GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE);
+
+    // Mapper les coordonnées de texture pour le plane
     plane.mapTexCoordsFromTexture(diffuseTexture);
 
-    // Set initial light position
-    lightPos = ofVec3f(300, 300, 500);
-
-    // Initialize the mapping method
     currentMappingMethod = DIFFUSE;
+    displacementScale = 5.0f;
 
-   // Load default shader (diffuse)
     currentShader.load("mapping/diffuse/shader");
 }
 
 void MappingScene::update()
 {
-    // Update light position (simple animation)
-    float time = ofGetElapsedTimef();
-    lightPos.x = 300 * sin(time * 0.5);
-    lightPos.z = 300 * cos(time * 0.5) + 500;
+
 }
 
 void MappingScene::draw()
 {
     plane.enableTextures();
     ofBackground(40);
-    
-    camera.begin();
 
+    camera.begin();
     if (ImGui::GetIO().WantCaptureMouse)
     {
         camera.disableMouseInput();
@@ -57,33 +75,57 @@ void MappingScene::draw()
         camera.enableMouseInput();
     }
 
-    // Draw light source
-    ofSetColor(255, 255, 0);
-    ofDrawSphere(lightPos, 20);
+    ofSetColor(255);
 
-    ofSetColor(255); 
     // Use shader and draw plane
     if (currentShader.isLoaded())
     {
+        currentShader.begin();
+
+        // Assignation des textures aux unités appropriées
         currentShader.setUniformTexture("diffuseTexture", diffuseTexture, 0);
 
         switch (currentMappingMethod)
         {
         case DISPLACEMENT:
             currentShader.setUniformTexture("heightMap", heightMap, 1);
+            currentShader.setUniform1f("displacementScale", displacementScale);
             break;
         case NORMAL:
-            currentShader.setUniformTexture("normalTexture", normalTexture, 2);
+            currentShader.setUniformTexture("normalTexture", normalTexture, 1);
+
+            // Envoyer les paramètres de lumière pour le normal mapping
+            currentShader.setUniform3f("lightDir", lightDirection.x, lightDirection.y, lightDirection.z);
+            currentShader.setUniform3f("lightColor", lightColor.x, lightColor.y, lightColor.z);
+            currentShader.setUniform1f("ambientStrength", ambientStrength);
             break;
-        case PARALLAX:
-            break;
+        case PARALLAX: break;
         }
 
-        currentShader.begin();
+        // Calcul et envoi des matrices au shader
+        ofMatrix4x4 modelViewMatrix = camera.getModelViewMatrix();
+        ofMatrix4x4 projectionMatrix = camera.getProjectionMatrix();
+        ofMatrix4x4 modelViewProjectionMatrix = projectionMatrix * modelViewMatrix;
 
+        currentShader.setUniformMatrix4f("modelViewMatrix", modelViewMatrix);
+        currentShader.setUniformMatrix4f("projectionMatrix", projectionMatrix);
+        currentShader.setUniformMatrix4f("modelViewProjectionMatrix", modelViewProjectionMatrix);
+
+        // Dessiner le plane
         ofPushMatrix();
         plane.draw();
         ofPopMatrix();
+
+        // Débinder les textures
+        if (currentMappingMethod == DISPLACEMENT)
+        {
+            heightMap.unbind(1);
+        }
+        else if (currentMappingMethod == NORMAL)
+        {
+            normalTexture.unbind(1);
+        }
+        diffuseTexture.unbind(0);
 
         currentShader.end();
     }
@@ -98,17 +140,44 @@ void MappingScene::draw()
         currentMappingMethod = static_cast<MappingMethod>(selectedOption);
         currentShader.load("mapping/" + std::string(mappingOptions[selectedOption]) + "/shader");
     }
+
+    // Ajouter un slider pour contrôler l'intensité du déplacement
+    if (currentMappingMethod == DISPLACEMENT)
+    {
+        float displacementScale = this->displacementScale;
+        if (ImGui::SliderFloat("Displacement Scale", &displacementScale, 0.0f, 100.0f))
+        {
+            this->displacementScale = displacementScale;
+        }
+    }
+
+    if (currentMappingMethod == NORMAL)
+    {
+        if (ImGui::TreeNode("Lighting Settings"))
+        {
+            // Direction de la lumière
+            float direction[3] = {lightDirection.x, lightDirection.y, lightDirection.z};
+            if (ImGui::SliderFloat3("Light Direction", direction, -1.0f, 1.0f))
+            {
+                lightDirection = glm::normalize(glm::vec3(direction[0], direction[1], direction[2]));
+            }
+
+            // Couleur de la lumière
+            float color[3] = {lightColor.x, lightColor.y, lightColor.z};
+            if (ImGui::ColorEdit3("Light Color", color))
+            {
+                lightColor = glm::vec3(color[0], color[1], color[2]);
+            }
+
+            // Force ambiante
+            if (ImGui::SliderFloat("Ambient Strength", &ambientStrength, 0.0f, 1.0f))
+            {
+                // La valeur est déjà mise à jour directement dans ambientStrength
+            }
+
+            ImGui::TreePop();
+        }
+    }
+
     ImGui::End();
 }
-
-void MappingScene::mousePressed(int x, int y, int button)
-{
-
-}
-
-void MappingScene::mouseDragged(int x, int y, int button)
-{
-
-}
-
-void MappingScene::mouseReleased(int x, int y, int button) { dragging = false; }
--- src/MappingScene.hpp
+++ src/MappingScene.hpp
@@ -1,48 +1,45 @@
 #pragma once
 
+#include "glm/glm.hpp"
 #include "ofMain.h"
 #include "ofxImGui.h"
 
 enum MappingMethod {
-    DIFFUSE,
-    DISPLACEMENT,
-    NORMAL,
-    PARALLAX
+   DIFFUSE,
+   DISPLACEMENT,
+   NORMAL,
+   PARALLAX
 };
 
 class MappingScene : public ofBaseApp {
 public:
-    void setup();
-    void update();
-    void draw();
+   void setup();
+   void update();
+   void draw();
 
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
+   // Primitive and camera
+   ofPlanePrimitive plane;
+   ofEasyCam camera;
 
-    // Primitive and camera
-    ofPlanePrimitive plane;
-    ofEasyCam camera;
+   // Variables for rotation
+   ofVec2f lastMouse;
+   ofQuaternion rotation;
+   ofQuaternion currentRotation;
+   bool dragging;
 
-    // Variables for rotation
-    ofVec2f lastMouse;
-    ofQuaternion rotation;
-    ofQuaternion currentRotation;
-    bool dragging;
+   // Normal mapping variables
+   ofShader currentShader;
+   ofTexture diffuseTexture;
+   ofTexture normalTexture;
+   ofTexture heightMap;
 
-    // Normal mapping variables
-    ofShader currentShader;
-    ofTexture diffuseTexture;
-    ofTexture normalTexture;
-    ofTexture heightMap;
+   // Parameters for displacement shader
+   float displacementScale = 0.0f; // Initialize to avoid undefined behavior
 
-    // Parallax parameters
-    float heightScale;
-    int numLayers;
-    bool useParallax;
+   MappingMethod currentMappingMethod;
 
-    // Light position
-    ofVec3f lightPos;
-
-    MappingMethod currentMappingMethod;
+   // Lighting parameters
+   glm::vec3 lightDirection = glm::vec3(0.0f, -1.0f, 0.0f);
+   glm::vec3 lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
+   float ambientStrength = 0.5f;
 };
